79caa49d54197b5305254639bdf443f7fec47753
Fedoseev Aleksandr <fedoseev@selectel.ru>
2020-11-26 21:03:33 +0300

    Homework 1 first attempt

--- hw01_hello_now/main.go
+++ hw01_hello_now/main.go
@@ -1,5 +1,19 @@
 package main
 
+import (
+	"fmt"
+	"log"
+	"time"
+
+	"github.com/beevik/ntp"
+)
+
 func main() {
-	// Place your code here
+	currentTime := time.Now()
+	fmt.Printf("current time: %s\n", currentTime.Format("2006-01-02 15:04:05 -0700 MST"))
+	NTPtime, err := ntp.Time("ntp3.stratum2.ru")
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("exact time: %s\n", NTPtime.Round(time.Second))
 }
