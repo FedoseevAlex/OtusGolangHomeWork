5c058f5a8d75f2046971f3859a02e8647efc6234
Fedoseev Aleksandr <fedoseev@selectel.ru>
2020-11-27 10:29:07 +0300

    Add reasonable spacing

--- hw01_hello_now/main.go
+++ hw01_hello_now/main.go
@@ -10,10 +10,12 @@ import (
 
 func main() {
 	currentTime := time.Now()
-	fmt.Printf("current time: %s\n", currentTime.Format("2006-01-02 15:04:05 -0700 MST"))
+
 	NTPtime, err := ntp.Time("ntp3.stratum2.ru")
 	if err != nil {
 		log.Fatal(err)
 	}
+
+	fmt.Printf("current time: %s\n", currentTime.Format("2006-01-02 15:04:05 -0700 MST"))
 	fmt.Printf("exact time: %s\n", NTPtime.Round(time.Second))
 }
