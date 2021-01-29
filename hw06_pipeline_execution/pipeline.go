6009fdca002696e06c4befccd6c0e31f2d5a0687
Fedoseev Aleksandr <fedoseev@selectel.ru>
2021-01-29 17:01:39 +0300

    Homework 6 is ready for review

--- hw06_pipeline_execution/pipeline.go
+++ hw06_pipeline_execution/pipeline.go
@@ -9,6 +9,32 @@ type (
 type Stage func(in In) (out Out)
 
 func ExecutePipeline(in In, done In, stages ...Stage) Out {
-	// Place your code here
-	return nil
+	for _, stage := range stages {
+		in = stage(makeClosableBroker(in, done))
+	}
+	return in
+}
+
+// makeClosableBroker creates a broker goroutine
+// which can be terminated via closing `interrupt` channel.
+func makeClosableBroker(in, interrupt In) Out {
+	out := make(Bi)
+
+	go func() {
+		defer close(out)
+
+		for {
+			select {
+			case data, ok := <-in:
+				if !ok {
+					return
+				}
+				out <- data
+			case <-interrupt:
+				return
+			}
+		}
+	}()
+
+	return out
 }
